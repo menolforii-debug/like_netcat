b3bb9a3e3ccfd564412da899b64f24a57ac5a1d6
menolforii-debug <menol.for.ii@gmail.com>
2026-02-04 14:56:44 +0300

    Use no-api-key TinyMCE CDN

--- app/admin/actions/post/object_delete.php
+++ app/admin/actions/post/object_delete.php
@@ -5,20 +5,24 @@ $sectionId = isset($_POST['section_id']) ? (int) $_POST['section_id'] : 0;
 if ($id > 0) {
     $object = $objectRepo->findById($id);
     if ($object === null) {
+        adminFlashSet('danger', 'Объект не найден');
         redirectTo(buildAdminUrl(['section_id' => $sectionId, 'tab' => 'content', 'error' => 'Объект не найден']));
     }
 
     $infoblock = $infoblockRepo->findById((int) $object['infoblock_id']);
     if ($infoblock === null) {
+        adminFlashSet('danger', 'Инфоблок не найден');
         redirectTo(buildAdminUrl(['section_id' => $sectionId, 'tab' => 'content', 'error' => 'Инфоблок не найден']));
     }
 
     if (!Permission::canAction($user, $infoblock, 'delete')) {
+        adminFlashSet('danger', 'Недостаточно прав');
         redirectTo(buildAdminUrl(['section_id' => $sectionId, 'tab' => 'content', 'error' => 'Недостаточно прав']));
     }
 
     $component = $componentRepo->findById((int) $object['component_id']);
     if ($component === null) {
+        adminFlashSet('danger', 'Компонент не найден');
         redirectTo(buildAdminUrl(['section_id' => $sectionId, 'tab' => 'content', 'error' => 'Компонент не найден']));
     }
 
@@ -42,5 +46,6 @@ if ($id > 0) {
     if ($user) {
         AdminLog::log($user['id'], 'object_delete', 'object', $id, []);
     }
+    adminFlashSet('success', 'Объект удален');
 }
 redirectTo(buildAdminUrl(['section_id' => $sectionId, 'tab' => 'content']));
